b26d50fdb6f99c35e0ceae15abbc9517e062a7fd
GuiValenteMM2 <guimartins.m.v@gmail.com>
2023-12-15 10:19:14 -0300

    Interface and layout done

---
+++ weather-app/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { RouterView } from 'vue-router';
+import Navigation from './components/Navigation.vue';
+</script>
+
+<template>
+  <div class="flex flex-col min-h-screen bg-weather-primary font-Roboto">
+    <Navigation />
+    <RouterView />
+  </div>
+</template>
+
